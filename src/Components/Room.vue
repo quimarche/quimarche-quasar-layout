d5bb5b73548aa5e1f0ce276aa83a674c86410cac
Chris Allison <chris@quimarche.co.uk>
2022-08-09 04:18:26 +0100

    Refactor room depending on activity

--- src/Components/Room.vue
+++ src/Components/Room.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-template(v-if="activity === 'Interaction'")
+template(v-if="['Interaction'].includes(activity)")
   div.column.fit
     div.col
       videos-recorded-participants(:videos="videos")
@@ -8,7 +8,13 @@ template(v-if="activity === 'Interaction'")
         div.col-2
           videos-unrecorded-participants(:videos="videos")
         div.col
-          div OTHER
+          div.bg-grey-3.fit {{ activity }}
+template(v-else-if="['Evaluation', 'Feedback', 'PreInteraction', 'Share'].includes(activity)")
+  div.fit.row
+    div.col-2
+      videos-all-participants(:videos="videos")
+    div.col
+      div.bg-grey-3.fit {{ activity }}
 template(v-else)
   div.fit.row
     videos-all-participants(:videos="videos")
