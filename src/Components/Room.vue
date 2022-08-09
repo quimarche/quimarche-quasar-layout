887ca7e9c3dbfd8382c0f7756dadc6b1ad4f8370
Chris Allison <chris@quimarche.co.uk>
2022-08-09 03:33:28 +0100

    Refactor Room to use Videos component

--- src/Components/Room.vue
+++ src/Components/Room.vue
@@ -1,34 +1,21 @@
 <template lang="pug">
-div#videos.fit
-  video(loop :src="video.src" v-for="video in videos")
+videos(:videos="videos")
 </template>
 
 <script>
+import Videos from '@/Components/Videos.vue'
+
 import UseObservers from '@/Uses/UseObservers'
 
 export default {
+  components: {
+    Videos
+  },
   props: {
     videos: {
       required: true,
       type: Array
     }
-  },
-  setup() {
-    const { observersVideosFinalise, observersVideosInitialise, observersVideosStyle } = UseObservers()
-    return {
-      observersVideosFinalise,
-      observersVideosInitialise,
-      observersVideosStyle
-    }
-  },
-  mounted() {
-    this.observersVideosInitialise('videos')
-    for (const element of document.getElementsByTagName('video')) {
-      setTimeout(() => element.play(), Math.floor(Math.random() * 1000))
-    }
-  },
-  beforeUnmount() {
-    this.observersVideosFinalise()
   }
 }
 </script>
